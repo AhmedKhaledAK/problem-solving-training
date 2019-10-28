069ae37968e4ceec936506a4a97450c68b550ac8
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-10-28 02:08:06 +0200

    added cpp solution for a problem

---
+++ codeforces/Integer Sequence Dividing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+#define array_size(a) (sizeof(a)/sizeof(a[0]))
+#define lpi(n) for(int i=0; i<n; i++)
+#define lpiv(v,n) for(int i=v; i<n; i++)
+#define lpj(n) for(int j=0; j<n; j++)
+#define iii pair<int,pair<int,int>>
+#define ii pair<int,int>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(int argc, const char * argv[]) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    cout << (n%4 == 0 || n%4 == 3 ? 0 : 1) << endl;
+    
+    return 0;
+}
+
