8d40d31e91b2a83b56dc817a97e75a9240aaf548
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-10-29 20:24:56 +0200

    added cpp solution for a problem

---
+++ codeforces/ThreePilesofCandies.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<bits/stdc++.h>
+ 
+#define array_size(a) (sizeof(a)/sizeof(a[0]))
+#define lpi(n) for(int i=0; i<n; i++)
+#define lpiv(v,n) for(int i=v; i<n; i++)
+#define lpj(n) for(int j=0; j<n; j++)
+#define iii pair<int,pair<int,int>>
+#define ii pair<int,int>
+ 
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main(int argc, const char * argv[]) {
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(0);
+    
+    int q;
+    ll o,t,th;
+    cin >> q;
+    while(q--){
+        cin >> o >> t >> th;
+        cout << (o+t+th)/2 << endl;
+    }
+    
+    return 0;
+}
