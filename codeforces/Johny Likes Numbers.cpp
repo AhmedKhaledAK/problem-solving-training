80167b10fa62c89ab4ed73d676d9aea9f4cd646e
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-12-02 21:10:53 +0200

    added cpp solution for a problem

---
+++ codeforces/Johny Likes Numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include <iomanip>
+ 
+#define array_size(a) (sizeof(a)/sizeof(a[0]))
+#define lpi(n) for(int i=0; i<n; i++)
+#define lpiv(v,n) for(int i=v; i<n; i++)
+#define lpj(n) for(int j=0; j<n; j++)
+#define iii pair<int,pair<int,int>>
+#define ii pair<int,int>
+ 
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main(int argc, const char * argv[]) {
+    
+    ll n,k;
+    cin >> n >> k;
+    
+    cout << (n / k + 1) * k << endl;
+    
+    
+    return 0;
+}
