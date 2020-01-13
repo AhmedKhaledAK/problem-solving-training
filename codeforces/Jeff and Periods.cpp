835d6de06a5cef021936ead9650f20d6bd546bf5
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-01-13 16:13:41 +0200

    added cpp solution for a problem

---
+++ codeforces/Jeff and Periods.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include <iomanip>
+ 
+#define array_size(a) (sizeof(a)/sizeof(a[0]))
+#define lpi(n) for(int i=0; i<n; i++)
+#define lpiv(v,n) for(int i=v; i<n; i++)
+#define lpj(n) for(int j=0; j<n; j++)
+#define lpjv(v,n) for(int j=v; j<n; j++)
+#define iii pair<int,pair<int,int>>
+#define ii pair<int,int>
+#define fi first
+#define se second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define vi vector<int>
+#define vip vector<pair<int,int>>
+#define pqig priority_queue<int>
+#define pqil priority_queue<int, vector<int>, greater<int>>
+#define filein(ff) freopen(ff, "r", stdin)
+#define fileout(ff) freopen(ff, "w", stdout)
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); 
+using namespace std;
+ 
+typedef long long ll;
+ 
+ 
+ll fastpow(ll nodess, ll num) {
+    if (!num)
+        return 1;
+    if (num % 2)
+        return nodess * fastpow(nodess, num - 1)/*%mod*/;
+    ll p = fastpow(nodess, num / 2);
+    // p %= mod;
+    p *= p;
+    // p %= mod;
+    return p;
+}
+ 
+ll my_mod(ll num, ll mod) {
+    ll reto = num % mod;
+    while (reto < 0)
+        reto += mod;
+    return reto;
+}
+ 
+ll gcd(ll u, ll v) { return !u ? v : gcd(v % u, u); }
+ 
+ll dist(pair<ll, ll> u, pair<ll, ll> v) {
+    return abs(u.fi - v.fi)  + abs(u.se - v.se);
+}
+ 
+ll lcm(ll a, ll b){
+    return a / __gcd(a, b) * b;
+}
+ 
+int diff[100001], last[100001], freq[100001]; 
+ 
+int main(int argc, const char * argv[]) {
+ 
+    fastio;
+    int n;
+    cin >> n;
+    int ar[n];
+    lpi(n)cin>>ar[i];
+ 
+    lpi(n){
+        if(last[ar[i]]!=-1){
+            if(freq[ar[i]]==0){
+                diff[ar[i]]=0;
+                last[ar[i]]=i;
+                freq[ar[i]]++;
+            } else if(freq[ar[i]]==1){
+                diff[ar[i]]=i-last[ar[i]];
+                last[ar[i]]=i;
+                freq[ar[i]]++;
+            } else{
+                if(i-last[ar[i]]!=diff[ar[i]]){
+                    last[ar[i]]=-1;
+                } else {
+                    last[ar[i]]=i;
+                }
+            }
+        }
+    }
+ 
+    vip pr;
+ 
+    lpi(n){
+        if(last[ar[i]]!=-1){
+            pr.pb({ar[i], diff[ar[i]]});
+            last[ar[i]]=-1;
+        }
+    }
+ 
+    sort(pr.begin(), pr.end());
+ 
+    cout << pr.size() << endl;
+ 
+    lpi(pr.size()){
+        cout << pr[i].fi << " " << pr[i].se << endl;
+    }
+ 
+    return 0; 
+}
