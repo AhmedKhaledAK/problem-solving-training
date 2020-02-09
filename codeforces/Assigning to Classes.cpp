ade773c4a634341f583f7bc2ebf25ba45b422022
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-02-09 20:19:27 +0200

    added cpp solution for a problem

---
+++ codeforces/Assigning to Classes.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+ 
+#define array_size(a) (sizeof(a)/sizeof(a[0]))
+#define lpi(n) for(int i=0; i<n; i++)
+#define lpiv(v,n) for(int i=v; i<n; i++)
+#define lpj(n) for(int j=0; j<n; j++)
+#define lpjv(v,n) for(int j=v; j<n; j++)
+#define iii pair<int,pair<int,int>>
+#define ii pair<int,int>
+#define fi first
+#define se second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define vi vector<int>
+#define vip vector<pair<int,int>>
+#define pqig priority_queue<int>
+#define pqil priority_queue<int, vector<int>, greater<int>>
+#define filein(ff) freopen(ff, "r", stdin)
+#define fileout(ff) freopen(ff, "w", stdout)
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); 
+ 
+using namespace std;
+ 
+typedef long long ll;
+ 
+ 
+ll fastpow(ll nodess, ll num) {
+    if (!num)
+        return 1;
+    if (num % 2)
+        return nodess * fastpow(nodess, num - 1)/*%mod*/;
+    ll p = fastpow(nodess, num / 2);
+    // p %= mod;
+    p *= p;
+    // p %= mod;
+    return p;
+}
+ 
+ll my_mod(ll num, ll mod) {
+    ll reto = num % mod;
+    while (reto < 0)
+        reto += mod;
+    return reto;
+}
+ 
+ll gcd(ll u, ll v) { return !u ? v : gcd(v % u, u); }
+ 
+ll dist(pair<ll, ll> u, pair<ll, ll> v) {
+    return abs(u.fi - v.fi)  + abs(u.se - v.se);
+}
+ 
+ll lcm(ll a, ll b){
+    return a / __gcd(a, b) * b;
+}
+ 
+int main(int argc, const char * argv[]) {
+ 
+    fastio;
+ 
+    int t,n,f,s;
+    cin >>t;
+    
+    while(t--){
+        cin >> n;
+        ll ar[2*n];
+ 
+        for(int i =0; i < 2*n; i++){
+            cin >> ar[i];
+        }
+ 
+        sort(ar,ar+2*n);
+ 
+        s=2*n/2;
+        f=s-1;
+ 
+        cout << ar[s]-ar[f] << endl;
+ 
+ 
+    }
+ 
+    return 0; 
+}   
