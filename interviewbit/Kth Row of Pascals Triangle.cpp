3723d04e69b291c18ad80fffdc0534f13f43e2ed
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-04-29 05:57:30 +0200

    added cpp solution for a problem

---
+++ interviewbit/Kth Row of Pascals Triangle.cpp
@@ -0,0 +1,19 @@
+vector<int> Solution::getRow(int A) {
+    vector<vector<int>> ans;
+    int j = 1;
+    for (int i = 0; i < A+1; i++) {
+        vector<int> v;
+        for (int k = 0; k < j; k++) {
+            if (k == 0 || k == j - 1) {
+                v.push_back(1);
+                continue;
+            }
+            int el = ans[i - 1][k] + ans[i - 1][k - 1];
+            v.push_back(el);
+        }
+        j++;
+        ans.push_back(v);
+    }
+    return ans[A];
+}
+
