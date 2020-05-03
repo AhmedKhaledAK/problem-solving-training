c5f5a79e378eaffafffa1380a2c898a9120714c0
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-05-03 16:59:08 +0200

    added cpp solution for a problem

---
+++ leetcode/Ransom Note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> freq;
+        for (char c : magazine) {
+            freq[c]++;
+        }
+        for (char c : ransomNote) {
+            if (--freq[c] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
