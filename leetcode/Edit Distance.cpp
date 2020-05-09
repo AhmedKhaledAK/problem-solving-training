c06a54e764e65e3bf07e2cd32a85002c98883632
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-05-09 05:37:08 +0200

    added cpp solution for a problem

---
+++ leetcode/Edit Distance.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.length(), m = word2.length();
+        vector<vector<int>> dp (n + 1, vector<int>(m + 1));
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min({dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]});
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
