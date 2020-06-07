54238252e8258f25c25349329153b699f0989be7
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-06-08 00:52:53 +0200

    added cpp solution for a problem

---
+++ leetcode/Coin Change 2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1);
+        dp[0] = 1;
+        for (int i = 0; i < coins.size(); i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j >= coins[i]) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+};
