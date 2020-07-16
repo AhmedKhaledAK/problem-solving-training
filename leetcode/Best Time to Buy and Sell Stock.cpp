fb71895c98fba7344c32e06e0bc5d26fe170de6e
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-07-17 01:35:26 +0200

    added cpp solution for a problem

--- leetcode/Best Time to Buy and Sell Stock.cpp
+++ leetcode/Best Time to Buy and Sell Stock.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0;
-        for(int i=1;i < prices.size(); i++){
-            if(prices[i] > prices[i-1])
-                profit+=prices[i]-prices[i-1];
+        int mn = INT_MAX, profit = 0;
+        for (int i = 0; i < (int) prices.size(); i++) {
+            if (mn > prices[i]) {
+                mn = prices[i];
+            } else if (prices[i] - mn > profit) {
+                profit = prices[i] - mn;
+            }
         }
         return profit;
     }
