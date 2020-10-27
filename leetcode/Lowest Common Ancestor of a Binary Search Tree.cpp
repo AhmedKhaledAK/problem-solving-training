4a17c9021824f4c837ffb83eef9d7db25f58d131
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-10-27 23:27:03 +0200

    added cpp solution for a problem

---
+++ leetcode/Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+    TreeNode *anc;
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        solve(root, p, q);
+        return anc;
+    }
+    bool solve(TreeNode *node, TreeNode *p, TreeNode *q) {
+        if (!node) {
+            return false;
+        }
+        
+        int left = solve(node->left, p, q) ? 1 : 0;
+        int right = solve(node->right, p, q) ? 1 : 0;
+        int mid = node->val == p->val || node->val == q->val ? 1 : 0;
+        
+        if (mid + left + right >= 2) {
+            anc = node;
+        }
+        
+        return mid + left + right > 0;
+    }
+};
