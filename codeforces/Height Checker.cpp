242f578ff2aeb62724d7246b8fd1a47b54811504
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2021-05-28 14:42:50 +0200

    added cpp solution for a problem

---
+++ codeforces/Height Checker.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> expected;
+        for (int h : heights) {
+            expected.push_back(h);
+        }
+        sort(expected.begin(), expected.end());
+        int ans = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != expected[i]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
