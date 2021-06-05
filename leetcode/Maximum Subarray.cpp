d33bd4d418ce280d142066d2d44fe524385cc84e
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2021-06-05 20:25:34 +0200

    added cpp solution for a problem

--- leetcode/Maximum Subarray.cpp
+++ leetcode/Maximum Subarray.cpp
@@ -11,3 +11,19 @@ public:
         return *max_element(dp, dp+nums.size());
     }
 };
+
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = (int) nums.size();
+        int ans = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < n; i++) {
+            sum = nums[i] + max(0, sum);
+            ans = max(sum, ans);
+        }
+        return ans;
+    }
+};
