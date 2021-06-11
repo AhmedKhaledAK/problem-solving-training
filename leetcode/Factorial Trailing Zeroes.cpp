3461d5e7bc5cf75e87d61dbc24328930a5d79b9f
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2021-06-11 13:25:27 +0200

    added cpp solution for a problem

---
+++ leetcode/Factorial Trailing Zeroes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        for (int d = 5; n/d >= 1; d *= 5) {
+            ans += n/d;
+        }
+        return ans;
+    }
+};
--- leetcode/Palindrome Partitioning.cpp
+++ leetcode/Palindrome Partitioning.cpp
@@ -29,3 +29,34 @@ public:
         return true;
     }
 };
+
+
+
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        int len = s.length();
+        vector<vector<bool>> dp(len, vector<bool>(len, false));
+        vector<vector<string>> result;
+        vector<string> current;
+        dfs(result, s, 0, current, dp);
+        return result;
+    }
+    
+    void dfs(vector<vector<string>>& result, string &s, int start, vector<string> &current, 
+            vector<vector<bool>> &dp) {
+        // the whole string is a palindrome
+        if (start >= s.length()) {
+            result.push_back(current);
+        }
+        for (int end = start; end < s.length(); end++) {
+            if (s[start] == s[end] && (end - start <= 2 || dp[start+1][end-1])) {
+                dp[start][end] = true;
+                current.push_back(s.substr(start, end - start + 1));
+                dfs(result, s, end + 1, current, dp);
+                current.pop_back();
+            }
+        }
+    }
+};
